c57f7788ba5a100cf708710cf15a523371db84ec
xxxsjan <626653354@qq.com>
2022-05-30 17:02:23 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
-import Vue from "vue";
-import App from "./App.vue";
-
-Vue.config.productionTip = false;
-
-new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+// import Vue from "vue";
+// import App from "./App.vue";
+// Vue.config.productionTip = false;
+// new Vue({
+//   render: (h) => h(App),
+// }).$mount("#app");
+export default {
+  install(Vue) {
+    Vue.directive("xxxdrag", {
+      bind: function (el) {
+        var oDiv = el;
+        oDiv.onmousedown = function (ev) {
+          var disX = ev.clientX - oDiv.offsetLeft;
+          var disY = ev.clientY - oDiv.offsetTop;
+          document.onmousemove = function (ev) {
+            var l = ev.clientX - disX;
+            var t = ev.clientY - disY;
+            oDiv.style.left = l + "px";
+            oDiv.style.top = t + "px";
+          };
+          document.onmouseup = function () {
+            document.onmousemove = null;
+            document.onmouseup = null;
+          };
+        };
+      },
+    });
+  },
+};
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  // 强制css内联
+  css: { extract: false },
 });
